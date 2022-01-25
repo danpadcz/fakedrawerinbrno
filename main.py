b5bac06d6eccfe3c37e547c005dac274039708b0
danpadcz <64928366+danpadcz@users.noreply.github.com>
2022-01-25 15:13:37 +0100

    initialize git and project

---
+++ main.py
@@ -0,0 +1,31 @@
+from random import randint
+from typing import Dict, List
+import sys
+import json
+
+
+def fake_artist_goes_to_ny(players: int, words: Dict[str, List[str]],
+                           gui: bool = False) -> None:
+    pass
+
+
+def parse_json(filename: str) -> Dict[str, List[str]]:
+    with open(filename, "r") as f:
+        loaded = f.read()
+
+    words: Dict[str, List[str]] = {}
+
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Player count not specified!")
+        return
+    if len(sys.argv) < 3:
+        print("json file with words not specified!")
+        return
+    words = parse_json(sys.argv[2])
+    fake_artist_goes_to_ny(int(sys.argv[1]))
+
+
+if __name__ == '__main__':
+    main()
