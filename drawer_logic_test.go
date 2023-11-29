c7c49f4f2647697215960e64f5afb9909a8f0949
Daniel Padrta <danielpadrta@gmail.com>
2023-11-29 17:15:29 +0100

    Implement CLI word adder tool
    This commit also makes the cli slightly different, where there now are two
    subprograms "play" and "add" which run the game and add words to json files.

--- drawer_logic_test.go
+++ drawer_logic_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 
@@ -22,7 +21,7 @@ func TestLogicNoWords(t *testing.T) {
 
 func TestLogicInvalidPlayerCount(t *testing.T) {
 	var w Words
-	w = append(w, map[string]string{"category": "animal", "text": "aardvark"})
+	w = append(w, Word{Word: "cat", Category: "animal"})
 	resultChan := make(chan result)
 	go FakeDrawerInBrnoLogic(w, -1, resultChan)
 
@@ -40,33 +39,9 @@ func TestLogicInvalidPlayerCount(t *testing.T) {
 	}
 }
 
-func TestLogicInvalidJSONCategory(t *testing.T) {
-	var w Words
-	w = append(w, map[string]string{"mooo": "animal", "text": "aardvark"})
-	resultChan := make(chan result)
-	go FakeDrawerInBrnoLogic(w, 1, resultChan)
-
-	r := <-resultChan
-	if assertions.ShouldBeError(r.Error, "invalid Json file format") != "" {
-		t.Fatalf(fmt.Sprintf("Expected 'invalid Json file format' error, got '%s'", r.Error))
-	}
-}
-
-func TestLogicInvalidJSONText(t *testing.T) {
-	var w Words
-	w = append(w, map[string]string{"category": "animal", "mooo": "aardvark"})
-	resultChan := make(chan result)
-	go FakeDrawerInBrnoLogic(w, 1, resultChan)
-
-	r := <-resultChan
-	if assertions.ShouldBeError(r.Error, errors.New("invalid Json file format")) != ""{
-		t.Fatalf(fmt.Sprintf("Expected 'invalid Json file format' error, got '%s'", r.Error))
-	}
-}
-
 func TestLogicValid(t *testing.T) {
 	var w Words
-	w = append(w, map[string]string{"category": "animal", "text": "aardvark"})
+	w = append(w, Word{Word: "cat", Category: "animal"})
 	resultChan := make(chan result)
 	go FakeDrawerInBrnoLogic(w, 3, resultChan)
 
@@ -89,8 +64,8 @@ func TestLogicValid(t *testing.T) {
 			impostorAppeared = true
 		} else if result.Message == "You are the fake :)" {
 			t.Fatalf("impostor appeared twice")
-		} else if result.Message != "The word is: aardvark" {
-			t.Fatalf("Given incorrect string, expected 'The word is: aardvark' gotten '%s'", result.Message)
+		} else if result.Message != "The word is: cat" {
+			t.Fatalf("Given incorrect string, expected 'The word is: cat' gotten '%s'", result.Message)
 		}
 
 	}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -15,55 +16,157 @@ import (
 )
 
 type CLIParser struct {
+	Play PlayParser `cmd:"" help:"Play A fake drawer in Brno"`
+	Add  AddParser  `cmd:"" help:"Add words to JSON file to be used in runs of the game"`
+}
+type PlayParser struct {
 	GUI         bool   `help:"Run in GUI"`
 	PlayerCount int    `arg:"" short:"c" help:"Amount of players. Must be greater than 0"`
 	JsonFile    string `arg:"" short:"f" help:"Path to JSON file to be used for words" type:"existingfile"`
 }
+type AddParser struct {
+	GUI      bool   `help:"Run in GUI"`
+	JsonFile string `arg:"" short:"f" help:"Path to JSON file to have words added" type:"existingfile"`
+}
 
-type Words []map[string]string
+type Words []Word
+type Word struct {
+	Word     string `json:"text"`
+	Category string `json:"category"`
+}
 
 func main() {
 	var cli CLIParser
-	err := kong.Parse(&cli, kong.Description("A game of lies and deceit... but with art!")).Error
-	if err != nil {
+	ctx := kong.Parse(&cli, kong.Description("A game of lies and deceit... but with art!"))
+	if ctx.Error != nil {
 		fmt.Println("Error occurred in parsing command line input")
 		os.Exit(1)
 	}
-
-	if cli.PlayerCount <= 0 {
-		fmt.Println("Player count must be specified and larger than 0!")
-		os.Exit(1)
-	}
-	if cli.JsonFile == "" {
-		fmt.Println("Json file with words must be specified!")
+	if err := ctx.Run(); err != nil {
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	f, err := os.ReadFile(cli.JsonFile)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	var w Words
-	err = json.Unmarshal(f, &w)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if cli.GUI {
-		if err := FakeDrawerInBrnoGUI(w, cli.PlayerCount); err != nil {
-			fmt.Println(err)
-		}
-	} else {
-		if err := FakeDrawerInBrnoCLI(w, cli.PlayerCount); err != nil {
-			fmt.Println(err)
-		}
-	}
 }
 
-// UI still very much placeholder, I want to add UI to add words,
-// make it nicer and less slideshow-like
+func (p *PlayParser) Run(ctx *kong.Context) error {
+	if p.PlayerCount <= 0 {
+		return errors.New("player count must be specified and larger than 0")
+	}
+	if p.JsonFile == "" {
+		return errors.New("json file with words must be specified")
+	}
+
+	w, err := loadJsonFile(p.JsonFile)
+	if err != nil {
+		return err
+	}
+
+	if p.GUI {
+		if err := FakeDrawerInBrnoGUI(w, p.PlayerCount); err != nil {
+			return err
+		}
+	} else {
+		if err := FakeDrawerInBrnoCLI(w, p.PlayerCount); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func loadJsonFile(path string) (Words, error) {
+	var w Words
+
+	f, err := os.ReadFile(path)
+	if err != nil {
+		return w, err
+	}
+
+	if err := json.Unmarshal(f, &w); err != nil {
+		return w, err
+	}
+	return w, nil
+}
+
+func saveToJsonFile(path string, w Words) error {
+	input, err := json.Marshal(w)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(path, input, 0o660); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *AddParser) Run(ctx *kong.Context) error {
+	return addWordsToJsonCLI(a.JsonFile)
+}
+
+func loadCategories(w Words) []string {
+	cats := make(map[string]bool)
+	for i := range w {
+		cats[w[i].Category] = true
+	}
+
+	result := make([]string, len(cats))
+	i := 0
+	for k := range cats {
+		result[i] = k
+		i++
+	}
+	return result
+}
+
+func addWordsToJsonCLI(path string) error {
+	w, err := loadJsonFile(path)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Categories in inputed JSON file: %s\n", loadCategories(w))
+
+	run := true
+	reader := bufio.NewReader(os.Stdin)
+
+	for run {
+		var toAdd Word
+		fmt.Println("Enter word to add: ")
+		temp, err := reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		toAdd.Word = string(temp)
+
+		fmt.Println("Enter category of word to add: ")
+		temp, err = reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		toAdd.Category = string(temp)
+
+		w = append(w, toAdd)
+
+		fmt.Println("Do you want to add another word? (Y/n) ")
+		temp, err = reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		input := string(temp)
+		if strings.Contains(input, "n") || strings.Contains(input, "N") {
+			run = false
+		} else if !strings.Contains(input, "y") && !strings.Contains(input, "Y") {
+			return errors.New("invalid input given")
+		}
+	}
+
+	if err := saveToJsonFile(path, w); err != nil {
+		return err
+	}
+	return nil
+}
+
 func FakeDrawerInBrnoGUI(w Words, playerCount int) error {
 	resultChan := make(chan result)
 	go FakeDrawerInBrnoLogic(w, playerCount, resultChan)
@@ -158,13 +261,8 @@ func FakeDrawerInBrnoLogic(w Words, playerCount int, out chan result) {
 	}
 	impostor := rand.Intn(playerCount)
 	selectedWord := rand.Intn(len(w))
-	category, catOk := w[selectedWord]["category"]
-	word, wordOk := w[selectedWord]["text"]
-
-	if !catOk || !wordOk {
-		out <- result{Error: errors.New("invalid Json file format")}
-		return
-	}
+	category := w[selectedWord].Category
+	word := w[selectedWord].Word
 
 	out <- result{Message: category}
 
